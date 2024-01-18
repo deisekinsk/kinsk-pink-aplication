518a8d5147d4f1398da1f59a36bc795c97c658aa
deisekinsk <kinsk@msn.com>
2024-01-18 16:58:05 -0300

    Start Project With Tree Package API REST

---
+++ src/main/java/com/kinsk/pink/PinkApplication.java
@@ -0,0 +1,13 @@
+package com.kinsk.pink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kinsk/pink/PinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinsk.pink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
