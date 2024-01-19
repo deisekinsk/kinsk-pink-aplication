66402ffbd70694b069aa5f85d29c52f3c233fac7
deisekinsk <kinsk@msn.com>
2024-01-19 02:24:25 -0300

    implement swagger documentation

--- src/main/java/com/kinsk/pink/config/SwaggerConfig.java
+++ src/main/java/com/kinsk/pink/config/SwaggerConfig.java
@@ -1,35 +1,26 @@
 //package com.kinsk.pink.config;
 //
-//
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import springfox.documentation.builders.ApiInfoBuilder;
-//import springfox.documentation.builders.PathSelectors;
-//import springfox.documentation.builders.RequestHandlerSelectors;
-//import springfox.documentation.service.ApiInfo;
-//import springfox.documentation.spi.DocumentationType;
-//import springfox.documentation.spring.web.plugins.Docket;
-//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//import org.springdoc.core.GroupedOpenApi;
+//import org.springdoc.core.SwaggerUiConfigParameters;
+//import org.springdoc.core.SwaggerUiConfigProperties;
 //
 //@Configuration
-//@EnableSwagger2
 //public class SwaggerConfig {
 //
 //    @Bean
-//    public Docket api() {
-//        return new Docket(DocumentationType.SWAGGER_2)
-//                .select()
-//                .apis(RequestHandlerSelectors.basePackage("com.kinsk.pink.controller"))
-//                .paths(PathSelectors.any())
-//                .build()
-//                .apiInfo(apiInfo());
+//    public GroupedOpenApi customApi() {
+//        return GroupedOpenApi.builder()
+//                .group("custom-api")
+//                .pathsToMatch("/api/custom/**")
+//                .build();
 //    }
 //
-//    private ApiInfo apiInfo() {
-//        return new ApiInfoBuilder()
-//                .title("Project Pink API")
-//                .description("PRODUCT USER SUBSCRIPTION")
-//                .version("1.0.0")
+//    @Bean
+//    public SwaggerUiConfigParameters swaggerUiConfigParameters() {
+//        return SwaggerUiConfigParametersBuilder.builder()
+//                .withDefaultUrl("/api/custom/api-docs")
 //                .build();
 //    }
 //}
